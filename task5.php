0d1aac9e9249b64be4d86967379a3076913501b5
lightsout93 <vjithlj[ez1>
2018-06-06 16:52:33 +0900

    homework_1

--- task5.php
+++ task5.php
@@ -4,6 +4,5 @@ $toyota = ['model' => 'Rav4', 'speed' => 100, 'doors' => 3, 'year' => '2016'];
 $opel = ['model' => 'astra', 'speed' => 110, 'doors' => 4, 'year' => '2014'];
 $cars = ['BMV' => $bmv, 'TOYOTA' => $toyota, 'OPEL' => $opel];
 foreach ($cars as $name => $value) {
-    //echo 'Car ' . $name . '<br>' . implode(' ', $value) . '<br>';
-    printf("Car: %s <br> %s %d %d %s<br>", $name, $value['model'], $value['speed'], $value['doors'], $value['year']);
+    echo 'Car ' . $name . '<br>' . implode(' ', $value) . '<br>';
 }
